c71ef7788838e32fd671cc58ddc32ae2dbe22d12
unknown <cspen@modintro.com>
2018-06-05 23:35:39 -0500

    Specify Linked List

---
+++ Lists/LinkedList.java
@@ -0,0 +1,30 @@
+/**
+ * Standard linked list.
+ *
+ * @author Craig Spencer <craigspencer@modintro.com>
+ *
+ */
+
+public class LinkedList {
+
+	public LinkedList() {
+	}
+
+	// Need to add basic functionality	
+
+	private static class Node {
+		ListNode() {
+			this.data = null;
+			this.next = null;
+		}
+
+		ListNode(Object data, ListNode next) {
+			this.data = date;
+			this.next = next;
+		}
+
+		ListNode(Object data) {
+			super(data, null);
+		}
+	}	
+}
