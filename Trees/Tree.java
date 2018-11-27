b30aa65729b0530a1edddc9960990808cbbfe9d3
unknown <cspen@modintro.com>
2018-11-26 23:47:32 -0600

    Work on treebuilder function

--- Trees/Tree.java
+++ Trees/Tree.java
@@ -353,39 +353,39 @@ public class Tree {
 		return null;
 	}
 
-/** 
-public TreeNode buildTree(int[] preorder, int[] inorder) {
-    int preStart = 0;
-    int preEnd = preorder.length-1;
-    int inStart = 0;
-    int inEnd = inorder.length-1;
+	/** 
+	public Tree buildTree(int[] preorder, int[] inorder) {
+    		int preStart = 0;
+    		int preEnd = preorder.length-1;
+    		int inStart = 0;
+    		int inEnd = inorder.length-1;
  
-    return construct(preorder, preStart, preEnd, inorder, inStart, inEnd);
-}
+    		return construct(preorder, preStart, preEnd, inorder, inStart, inEnd);
+	}
  
-public TreeNode construct(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd){
-    if(preStart>preEnd||inStart>inEnd){
-        return null;
-    }
+	public Tree construct(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd){
+    		if(preStart>preEnd||inStart>inEnd){
+        		return null;
+    		}
  
-    int val = preorder[preStart];
-    TreeNode p = new TreeNode(val);
+    		int val = preorder[preStart];
+    		TreeNode p = new Tree(val);
  
-    //find parent element index from inorder
-    int k=0;
-    for(int i=0; i<inorder.length; i++){
-        if(val == inorder[i]){
-            k=i;
-            break;
-        }
-    }
+    		//find parent element index from inorder
+    		int k=0;
+    		for(int i=0; i<inorder.length; i++) {
+        		if(val == inorder[i]) {
+            			k=i;
+            			break;
+        		}
+    		}
  
-    p.left = construct(preorder, preStart+1, preStart+(k-inStart), inorder, inStart, k-1);
-    p.right= construct(preorder, preStart+(k-inStart)+1, preEnd, inorder, k+1 , inEnd);
+    		p.left = construct(preorder, preStart+1, preStart+(k-inStart), inorder, inStart, k-1);
+    		p.right= construct(preorder, preStart+(k-inStart)+1, preEnd, inorder, k+1 , inEnd);
  
-    return p;
-}
-*/
+    		return p;
+	}
+	*/
 
 
 
