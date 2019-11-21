5b1a32a98319360d0800b1f71b9e09491ccf542e
unknown <cspen@modintro.com>
2019-11-21 02:16:56 -0600

    Implement and test simple bubble sort

---
+++ Sorting/Bubble.java
@@ -0,0 +1,31 @@
+// Classic Bubble sort
+import java.util.*;
+
+public class Bubble {
+
+	public static void main(String[] args) {
+		int[] theArr = new int[20];
+		System.out.println(Arrays.toString(theArr));
+		randomFill(theArr);
+		System.out.println(Arrays.toString(theArr));
+		sort(theArr);
+		System.out.println(Arrays.toString(theArr));
+	}
+
+	public static void sort(int[] a) {
+		for(int i = 0; i < a.length-1; i++)
+			for(int j = i+1; j < a.length; j++) {
+				if(a[i] > a[j]) {
+					int t = a[i];
+					a[i] = a[j];
+					a[j] = t;
+				}
+			}
+	}
+
+	public static void randomFill(int[] a) {
+		for(int i = 0; i < a.length; i++) {
+			a[i] = (int)(Math.random() * 100 + 1);
+		}
+	}
+}
