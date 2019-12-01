01ff9d07a7cd37d4f4683b4483699105174b98ea
unknown <cspen@modintro.com>
2019-11-30 23:56:09 -0600

    Implement classic selection sort

---
+++ Sorting/Selection.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Selection {
+
+	public static void main(String[] args) {
+		int[] a = new int[20];
+	}
+
+	public static void selection(int[] a) {
+		int min = i;
+		for(int i = 0; i < a.length; i++) {
+			for(int j = i+1; j < a.length; j++) {
+			}
+		} 
+	}
+
+	
+
+}
