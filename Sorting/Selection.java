f28e7f05a356d5780ff294178573eb8a10988843
unknown <cspen@modintro.com>
2019-12-01 00:10:21 -0600

    Finish implement classic selection sort

--- Sorting/Selection.java
+++ Sorting/Selection.java
@@ -7,11 +7,26 @@ public class Selection {
 	}
 
 	public static void selection(int[] a) {
-		int min = i;
+		
 		for(int i = 0; i < a.length; i++) {
+			int min = i;
 			for(int j = i+1; j < a.length; j++) {
+				if(a[j] < a[min])
+					min = j;
+			}
+			if(min != i) {
+				temp = a[i];
+				a[i] = a[min];
+				a[min] = temp;
 			}
 		} 
+		
+	}
+
+	public static void randomFill(int[] a) {
+		for(int i = 0; i < a.length; i++) {
+			a[i] = (int)(Math.random() * 100 + 1);
+		}
 	}
 
 	
