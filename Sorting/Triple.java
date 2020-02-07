f92a588f6afff27049d4e6a7c476fd4bd57e8791
unknown <cspen@modintro.com>
2020-02-06 23:54:52 -0600

    Study for test

---
+++ Sorting/Triple.java
@@ -0,0 +1,13 @@
+
+
+public class Triple {
+
+	public static void main(String[] args) {
+
+		int i;
+		for(i = 0; i < 10; i++) {}
+		int x = i;
+
+		System.out.println(x);
+	}
+}
---
+++ Tests/FireBreather.java
@@ -0,0 +1,3 @@
+public interface FireBreather extends Animal {
+	
+}
